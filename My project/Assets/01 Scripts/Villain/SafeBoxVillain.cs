30058bde3448cadc12701027ddd335a996380771
yugd1211 <yugd1211@gmail.com>
2025-02-04 19:25:32 +0900

    [REFACTOR] : delete SearchSafebox function Villain의 SearchInteractive으로 대체

--- My project/Assets/01 Scripts/Villain/SafeBoxVillain.cs
+++ My project/Assets/01 Scripts/Villain/SafeBoxVillain.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SafeBoxVillain : Villain
@@ -25,41 +22,26 @@ public class SafeBoxVillain : Villain
             return;
         base.Update();
 
-        if (SearchSafeBox(out SafeBox safeBox))
+        if (SearchInteractive(out SafeBox safeBox))
         {
+            if (isDestroy) 
+                return;
             safeBox.isInteractable = false;
             _safeBoxSearchTime += Time.deltaTime;
         }
         else
-        {
             _safeBoxSearchTime = 0;
-            
-        }
         if (_safeBoxSearchTime >= safeBoxDeleteTime)
         {
             safeBox.HalveMoney();
-            Destroy(gameObject);
+            Destroy();
         }
     }
     
     protected override void Destroy()
     {
+        base.Destroy();
         isDestroy = true;
         GameManager.Instance.safeBox.isInteractable = true;
-        base.Destroy();
-    }
-
-    private bool SearchSafeBox(out SafeBox safeBox)
-    {
-        safeBox = null;
-        foreach (Vector2 dir in SearchDirs)
-        {
-            RaycastHit2D hit = Physics2D.Raycast(
-                transform.position, dir, 1f, 
-                LayerMask.GetMask(LayerName.Interactive.ToString()));
-            if (hit.collider.TryGetComponent(out safeBox))
-                return true;
-        }
-        return false;
     }
 }
